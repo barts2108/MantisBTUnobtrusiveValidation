82a46f7a6c2af1199840b8b8328171ed21a83fe5
barts2108 <barts2108@yahoo.co.uk>
2026-01-27 10:44:44 +0100

    Bump to V1.0.2

--- MantisBTUnobtrusiveValidation.php
+++ MantisBTUnobtrusiveValidation.php
@@ -28,7 +28,7 @@ extends MantisPlugin
 		$this->name =  plugin_lang_get("title");
 		$this->description = plugin_lang_get("description");
 
-		$this->version = '1.0.1';
+		$this->version = '1.0.2';
 		$this->requires = array(
 			'MantisCore' => '2.0.0',
 		);
